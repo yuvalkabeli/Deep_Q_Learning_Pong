4523caf5b88cd2ef53e9c20ea10dbf5f4d623787
yuvalkabeli <yuvalkabeli@gmail.com>
2024-04-14 22:43:05 +0300

    first working model!

--- dqn_keras.py
+++ dqn_keras.py
@@ -1,137 +1,132 @@
-from keras.layers import  Dense, Conv2D,Flatten
+from keras.layers import Dense, Activation, Conv2D, Flatten
 from keras.models import Sequential, load_model
 from keras.optimizers import Adam
 from keras.utils import disable_interactive_logging
-from keras import backend as K
+from tensorflow import debugging, config
 import numpy as np
 
 class ReplayBuffer(object):
     def __init__(self, max_size, input_shape):
+        
         self.mem_size = max_size
         self.mem_cntr = 0
 
-        self.state_memory = np.zeros((self.mem_size, *input_shape),dtype=np.float32)
-
-        self.new_state_memory = np.zeros((self.mem_size, *input_shape),dtype=np.float32)
-        
+        self.state_memory = np.zeros((self.mem_size, *input_shape),
+                                      dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape),
+                                          dtype=np.float32)
         self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.uint8)
 
-        self.reward_memory = np.zeros(self.mem_size,dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size,dtype=np.uint8)
-    
     def store_transition(self, state, action, reward, state_, done):
         if state[0].shape==(4,80,80):
             state=state[0]
         index = self.mem_cntr % self.mem_size
         self.state_memory[index] = state
         self.new_state_memory[index] = state_
-        self.reward_memory[index] = reward
         self.action_memory[index] = action
+        self.reward_memory[index] = reward
         self.terminal_memory[index] = done
-
-        self.mem_cntr+=1
+        self.mem_cntr += 1
 
     def sample_buffer(self, batch_size):
-        max_mem = min(self.mem_cntr,self.mem_size)
-        batch = np.random.choice(max_mem,batch_size,replace=False)
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, batch_size, replace=False)
 
         states = self.state_memory[batch]
-        new_states = self.new_state_memory[batch]
         actions = self.action_memory[batch]
         rewards = self.reward_memory[batch]
-        dones = self.terminal_memory[batch]
+        states_ = self.new_state_memory[batch]
+        terminal = self.terminal_memory[batch]
 
-        return states, actions, rewards, new_states, dones
-    
-def build_dqn(lr, n_actions,input_dims, fcl_dims):
+        return states, actions, rewards, states_, terminal
 
-    model = Sequential([
-        Conv2D(filters=32,kernel_size=8,strides=4, activation='relu',
-                    input_shape=(*input_dims,), data_format='channels_first'),
-        Conv2D(filters=64,kernel_size=4,strides=2, activation='relu',
-                    input_shape=(*input_dims,), data_format='channels_first'),
-        Conv2D(filters=64,kernel_size=3,strides=1, activation='relu',
-                    input_shape=(*input_dims,), data_format='channels_first'),
-        Flatten(),
-        Dense(fcl_dims,activation='relu'),
-        Dense(n_actions)
+def build_dqn(lr, n_actions, input_dims, fc1_dims):
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=8, strides=4, activation='relu',
+                     input_shape=(*input_dims,), data_format='channels_first'))
+    model.add(Conv2D(filters=64, kernel_size=4, strides=2, activation='relu',
+                     data_format='channels_first'))
+    model.add(Conv2D(filters=64, kernel_size=3, strides=1, activation='relu',
+                     data_format='channels_first'))
+    model.add(Flatten())
+    model.add(Dense(fc1_dims, activation='relu'))
+    model.add(Dense(n_actions))
 
-    ])
-
-    model.compile(optimizer=Adam(learning_rate=lr), loss = 'mean_squared_error')
+    model.compile(optimizer=Adam(learning_rate=lr), loss='mean_squared_error')
 
     return model
 
 class Agent(object):
     def __init__(self, alpha, gamma, n_actions, epsilon, batch_size, replace,
-                 input_dims,eps_dec=1e-5, eps_min=0.01, mem_size=1000000,
-                 q_eval_fname='q_eval.keras',q_target_fname='q_target.keras'):
+                 input_dims, eps_dec=0.996,  eps_min=0.01,
+                 mem_size=1000000, q_eval_fname='q_eval.h5',
+                 q_target_fname='q_next.h5'):
         self.action_space = [i for i in range(n_actions)]
         self.gamma = gamma
         self.epsilon = epsilon
         self.eps_dec = eps_dec
         self.eps_min = eps_min
-        self.batch_size=batch_size
-        self.replace=replace
-        self.q_target_model_file=q_target_fname
-        self.q_eval_model_file=q_eval_fname
+        self.batch_size = batch_size
+        self.replace = replace
+        self.q_target_model_file = q_target_fname
+        self.q_eval_model_file = q_eval_fname
         self.learn_step = 0
-        self.memory = ReplayBuffer(mem_size,input_dims)
-        self.q_eval = build_dqn(alpha,n_actions,input_dims,512)
-        self.q_next = build_dqn(alpha,n_actions,input_dims,512)
         disable_interactive_logging()
-    
+        # debugging.set_log_device_placement(True)
+        config.experimental_run_functions_eagerly(True)
+        self.memory = ReplayBuffer(mem_size, input_dims)
+        self.q_eval = build_dqn(alpha, n_actions, input_dims, 512)
+        self.q_next = build_dqn(alpha, n_actions, input_dims, 512)
+
     def replace_target_network(self):
-        if self.replace !=0 and self.learn_step % self.replace == 0:
+        if self.replace is not None and self.learn_step % self.replace == 0:
             self.q_next.set_weights(self.q_eval.get_weights())
 
-    def store_transition(self,state,action,reward,new_state,done):
-        self.memory.store_transition(state,action,reward,new_state,done)
+    def store_transition(self, state, action, reward, new_state, done):
+        self.memory.store_transition(state, action, reward, new_state, done)
 
-    def choose_action(self,observation):
+    def choose_action(self, observation):
         if np.random.random() < self.epsilon:
             action = np.random.choice(self.action_space)
         else:
             if observation[0][0].shape==(80,80):
                 observation=observation[0]
-            state = np.array([observation],copy=False, dtype=np.float32)
+            state = np.array([observation], copy=False, dtype=np.float32)
             actions = self.q_eval.predict(state)
             action = np.argmax(actions)
 
         return action
-    
+
     def learn(self):
         if self.memory.mem_cntr > self.batch_size:
-            state,action,reward,new_state,done = \
-                                self.memory.sample_buffer(self.batch_size)
-                            
+            state, action, reward, new_state, done = self.memory.sample_buffer(self.batch_size)
+
             self.replace_target_network()
 
             q_eval = self.q_eval.predict(state)
-            q_next = self.q_next.predict(new_state)
 
-            q_next[done] = 0.0
+            q_next = self.q_next.predict(new_state) 
 
+            q_target = np.array(q_eval)
             indices = np.arange(self.batch_size)
-            q_target  =q_eval[:]
-            
-            q_target[indices,action] = reward + \
-                                                self.gamma*np.max(q_next,axis=1)
-            
-            self.q_eval.fit(state,q_target,verbose=0)
+            q_target[indices, action] = reward + self.gamma * np.max(q_next, axis=1) * (1 - done)
 
-            self.epsilon = self.epsilon - self.eps_dec  \
-                            if self.epsilon > self.eps_min else self.eps_min
+            try:
+                self.q_eval.train_on_batch(state, q_target)
+            except BaseException as e:
+                print("Exception during training:", e)
 
+            self.epsilon = max(self.epsilon - self.eps_dec, self.eps_min)
             self.learn_step += 1
-        
+
     def save_models(self):
         self.q_eval.save(self.q_eval_model_file)
         self.q_next.save(self.q_target_model_file)
-        print('...saving models...')
+        print('... saving models ...')
 
     def load_models(self):
-        self.q_eval=load_model(self.q_eval_model_file)
-        self.q_next=load_model(self.q_target_model_file)
-        print('...loading models...')
-
+        self.q_eval = load_model(self.q_eval_model_file)
+        self.q_nexdt = load_model(self.q_target_model_file)
+        print('... loading models ...')
--- pong_main_env.py
+++ pong_main_env.py
@@ -1,52 +1,56 @@
-import numpy as np  
+import numpy as np
 from dqn_keras import Agent
-from utils import plotLearning, make_env 
+from utils import  make_env
 
 if __name__ == '__main__':
-    env = make_env("ALE/Pong-v5")
-    num_games = 500
-    load_checkpoint = False
-    best_score =-21
-    agent = Agent(gamma=0.99,epsilon=1.0,alpha=0.0001,input_dims=(4,80,80),
-                  n_actions=6, mem_size=25000, eps_min=0.02,batch_size=32,
-                  replace=1000,eps_dec=1e-5)
-    # if load_checkpoint:
-    agent.load_models()
+    env = make_env('ALE/Pong-v5')
 
-    scores,eps_history =[],[]
+    num_games = 700
+    load_checkpoint = False
+    best_score = -21
+    agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0001,
+                  input_dims=(4,80,80), n_actions=6, mem_size=25000,
+                  eps_min=0.02, batch_size=32, replace=1000, eps_dec=1e-5)
+
+    if load_checkpoint:
+        agent.load_models()
+
+    filename = 'PongNoFrameskip-v4.png'
+
+    scores, eps_history = [], []
     n_steps = 0
 
     for i in range(num_games):
-        score = 0
+        done = False
         observation = env.reset()
-
-        done=False
+        score = 0
         while not done:
             action = agent.choose_action(observation)
-            
-            observation_,reward,terminated, truncated,info = env.step(action)
+            observation_, reward, terminated, truncated, info = env.step(action)
             done = terminated or truncated
-            n_steps+=1
-            score+=reward
+            n_steps += 1
+            score += reward
             if not load_checkpoint:
-                agent.store_transition(observation,action,reward,observation_,int(done))
+                agent.store_transition(observation, action,
+                                     reward, observation_, int(done))
                 agent.learn()
             else:
                 env.render()
-
             observation = observation_
+
         scores.append(score)
 
         avg_score = np.mean(scores[-100:])
-        print('episode ', i,'score ', score, 'average score %.2f' % avg_score,
-               'epsilon %.2f' % agent.epsilon, 'steps',n_steps)
+        print('episode: ', f"{i}/{num_games}",'score: ', score,
+             ' average score %.3f' % avg_score,
+            'epsilon %.2f' % agent.epsilon, 'steps', n_steps)
         if avg_score > best_score:
             agent.save_models()
-            print('avg score %.2f better than best score %.2f' % (avg_score,best_score))
+            print('avg score %.2f better than best score %.2f, saving model' % (
+                  avg_score, best_score))
             best_score = avg_score
 
         eps_history.append(agent.epsilon)
 
-    print(f"best score:{best_score}")
-
     x = [i+1 for i in range(num_games)]
+    # plot_learning_curve(x, scores, eps_history, filename)
